4ee7103065de39e7ee224eae3bd8f5ffe150a791
arpat1 <mvr0636@gmail.com>
2025-11-01 17:54:42 +0400

    Make tickets count statable to change its value by setState

--- src/components/Tickets/Tickets.jsx
+++ src/components/Tickets/Tickets.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react"
 import styles from "./Tickets.module.css"
 
 const Tickets = () => {
+    const [tickCount, setTickCount] = useState(3)
     return (
         <header>
             <div className={styles["tickets"]}>
                 <img src="src/assets/ticket_icon.svg" width="15px" height="15px" className={styles["tick-img"]}/>
-                <span className={styles["tick-count"]}>3</span>
+                <span className={styles["tick-count"]}>{tickCount}</span>
             </div>
         </header>
     )
