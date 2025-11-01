9ea67499e9e9760dfbe662c01ab0e3d0c49c2cfb
arpat1 <mvr0636@gmail.com>
2025-11-01 17:51:58 +0400

    Add content and style Tickets

--- src/components/Tickets/Tickets.jsx
+++ src/components/Tickets/Tickets.jsx
@@ -1,8 +1,13 @@
-import styles from ",/Tickets.module.css"
+import styles from "./Tickets.module.css"
 
 const Tickets = () => {
     return (
-        <></>
+        <header>
+            <div className={styles["tickets"]}>
+                <img src="src/assets/ticket_icon.svg" width="15px" height="15px" className={styles["tick-img"]}/>
+                <span className={styles["tick-count"]}>3</span>
+            </div>
+        </header>
     )
 }
 
