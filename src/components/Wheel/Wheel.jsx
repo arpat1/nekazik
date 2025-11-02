454adf8a869dbb2cd531e1bf39739422d2cd75c4
arpat1 <mvr0636@gmail.com>
2025-11-02 15:05:06 +0400

    Add prizes list

--- src/components/Wheel/Wheel.jsx
+++ src/components/Wheel/Wheel.jsx
@@ -5,6 +5,7 @@ import { useState } from "react"
 const Wheel = () => {
     const wheel = useRef(0)
     const [clickCount, setClickCount] = useState(0)
+    const prizes = ["ferrari", "playstation 5", "anhayt priz ?", "17 pro max", "kvartira", "3d printer", "ployka/tenis", "200$"]
     const controlPrizes = [50, 1307, 808]
     const onClickHandle = () => {
         if (clickCount != 3){
@@ -18,7 +19,7 @@ const Wheel = () => {
             <div className={styles["wheel"]} onClick={onClickHandle} ref={wheel}>
                 {[...Array(4)].map((_, i) => <span key={i} style={{"--i": i}}></span>)}
                 <div className={styles["number"]}>
-                    {[...Array(8)].map((_, i) => <b key={i} style={{"--i": i}}>{i}</b>)}
+                    {prizes.map((prize, i) => <b key={i} style={{"--i": i}}>{prize}</b>)}
                 </div>
             </div>
         </div>
