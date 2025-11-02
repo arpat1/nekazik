d13245dd5d6a3b6db92ae971275e1a6305922ce3
arpat1 <mvr0636@gmail.com>
2025-11-02 14:22:15 +0400

    Style and make interactive for wheel

--- src/components/Wheel/Wheel.jsx
+++ src/components/Wheel/Wheel.jsx
@@ -1,8 +1,21 @@
+import { useRef } from "react"
 import styles from "./Wheel.module.css"
 
 const Wheel = () => {
+    const wheel = useRef(0)
+    const onClickHandle = () => {
+        let rotate_angle = Math.ceil(Math.random() * 3600)
+        wheel.current.style.transform = `rotate(${rotate_angle}deg)` 
+    }
     return (
-        <></>
+        <div className={styles["container"]}>
+            <div className={styles["wheel"]} onClick={onClickHandle} ref={wheel}>
+                {[...Array(4)].map((_, i) => <span key={i} style={{"--i": i}}></span>)}
+                <div className={styles["number"]}>
+                    {[...Array(8)].map((_, i) => <b key={i} style={{"--i": i}}>{i}</b>)}
+                </div>
+            </div>
+        </div>
     )
 }
 
